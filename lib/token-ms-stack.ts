2762b159a91965c1b443145e17348adc8eadfc57
griffinodow <griffin@griffindow.com>
2022-11-19 17:01:32 -0800

    Cleanup unused code

--- lib/token-ms-stack.ts
+++ lib/token-ms-stack.ts
@@ -2,18 +2,14 @@ import * as cdk from "aws-cdk-lib";
 import * as path from "path";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { HostedZone } from "aws-cdk-lib/aws-route53";
 import { Construct } from "constructs";
 import {
-  DomainName,
   EndpointType,
   LambdaIntegration,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
 import { CfnApiMapping } from "aws-cdk-lib/aws-apigatewayv2";
-import { DnsValidatedCertificate } from "aws-cdk-lib/aws-certificatemanager";
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TokenMsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
